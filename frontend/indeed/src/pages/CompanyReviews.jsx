818844c66b38876d9bb4a464e6960152adaccdaa
srishti-23 <142992320+srishti-23@users.noreply.github.com>
2024-10-01 14:16:50 +0530

    Update CompanyReviews.jsx

--- frontend/indeed/src/pages/CompanyReviews.jsx
+++ frontend/indeed/src/pages/CompanyReviews.jsx
@@ -12,7 +12,7 @@ const CompanyReviews = () => {
 
   useEffect(() => {
     fetch(
-      `http://api.adzuna.com/v1/api/jobs/gb/top_companies?app_id=7ba37328&app_key=5cd06c669e15639b874c09749126949e&what=cook&content-type=application/json`
+      `https://api.adzuna.com/v1/api/jobs/gb/top_companies?app_id=7ba37328&app_key=5cd06c669e15639b874c09749126949e&what=cook&content-type=application/json`
     )
       .then((response) => response.json())
       .then((data) => {
