19eb38a1e5981e03a20a08a952b40193b9c7daf3
srishti-23 <142992320+srishti-23@users.noreply.github.com>
2024-11-16 20:01:08 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173", 
     "https://indeed1.netlify.app/",
-     "https://api.adzuna.com/v1/api/jobs/gb/top_companies?app_id=7ba37328&app_key=5cd06c669e15639b874c09749126949e&what=cook&content-type=application/json
+    "https://api.adzuna.com/v1/api/jobs/gb/top_companies?app_id=7ba37328&app_key=5cd06c669e15639b874c09749126949e&what=cook&content-type=application/json,
     
     methods: 'GET,POST,PUT,PATCH,DELETE',
     credentials: true
