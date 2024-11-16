1363468a270d686a9f81c0ef0c7ac1aa0d57a6e2
srishti-23 <142992320+srishti-23@users.noreply.github.com>
2024-11-16 19:03:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+app.listen(PORT,0.0.0.0, () => {
     connectDb();
     console.log(`Server listening at ${PORT}`);
 });
