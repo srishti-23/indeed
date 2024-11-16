a7bad229ba080299d390fc8ab9184fd5d3dcb4b4
srishti-23 <142992320+srishti-23@users.noreply.github.com>
2024-11-16 18:41:02 +0530

    Update BookmarkContext.jsx

--- frontend/indeed/src/contexts/BookmarkContext.jsx
+++ frontend/indeed/src/contexts/BookmarkContext.jsx
@@ -8,8 +8,6 @@ function reducer(state, action) {
     case "SET_BOOKMARKS":
       return { ...state, bookmarks: action.payload || [] };
 
-    // case "ADD_BOOKMARK":
-    //   return { ...state, bookmarks: [...state.bookmarks, action.payload] };
     case "ADD_BOOKMARK":
       console.log(
         "Current bookmarks:",
@@ -19,13 +17,6 @@ function reducer(state, action) {
       );
       return { ...state, bookmarks: [...state.bookmarks, action.payload] };
 
-    // case "REMOVE_BOOKMARK":
-    //   return {
-    //     ...state,
-    //     bookmarks: state.bookmarks.filter(
-    //       (bookmark) => bookmark.id !== action.payload.id
-    //     ),
-    //   };
     case "REMOVE_BOOKMARK":
       return {
         ...state,
@@ -34,23 +25,7 @@ function reducer(state, action) {
         ),
       };
 
-    // case "TOGGLE_BOOKMARK":
-    //   const isBookmarked = state.bookmarks.some(
-    //     (bookmark) => bookmark.id === action.payload.id
-    //   );
-    //   if (isBookmarked) {
-    //     return {
-    //       ...state,
-    //       bookmarks: state.bookmarks.filter(
-    //         (bookmark) => bookmark.id !== action.payload.id
-    //       ),
-    //     };
-    //   } else {
-    //     return {
-    //       ...state,
-    //       bookmarks: [...state.bookmarks, action.payload],
-    //     };
-    //   }
+    
     case "TOGGLE_BOOKMARK":
       const isBookmarked = state.bookmarks.some(
         (bookmark) => bookmark._id === action.payload._id
@@ -84,7 +59,7 @@ export const BookmarksProvider = ({ children }) => {
     async function fetchBookmarks() {
       try {
         const response = await axios.get(
-          "https://localhost:8080/api/bookmark/details",
+          "https://indeed-vmus.onrender.com/api/bookmark/details",
           { withCredentials: true }
         );
         console.log("response.data", response.data);
